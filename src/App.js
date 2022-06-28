0508ed70192bae328dc8cca4a36591594e6ce2c6
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-06-28 16:07:25 +0530

    Update with new categories compenents and stylings

--- src/App.js
+++ src/App.js
@@ -1,42 +1,9 @@
+import Category from "./components/categories/category.component";
+import categories from "./components/categoryData/categories";
+
 const App = () => {
   return (
-    <div className="categories-containers">
-      <div className="categroy-container">
-        {/* <img/> */}
-        <div className="category-body-conatiner">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="categroy-container">
-        {/* <img/> */}
-        <div className="category-body-conatiner">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="categroy-container">
-        {/* <img/> */}
-        <div className="category-body-conatiner">
-          <h2>Sneaker</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="categroy-container">
-        {/* <img/> */}
-        <div className="category-body-conatiner">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="categroy-container">
-        {/* <img/> */}
-        <div className="category-body-conatiner">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
+    <Category categories={categories}></Category>
   );
 };
 
