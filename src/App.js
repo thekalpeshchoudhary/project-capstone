0603dee5f4069b8926d8c29e6208f23200be885d
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-06-28 15:06:09 +0530

    Project Base setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-containers">
+      <div className="categroy-container">
+        {/* <img/> */}
+        <div className="category-body-conatiner">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categroy-container">
+        {/* <img/> */}
+        <div className="category-body-conatiner">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categroy-container">
+        {/* <img/> */}
+        <div className="category-body-conatiner">
+          <h2>Sneaker</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categroy-container">
+        {/* <img/> */}
+        <div className="category-body-conatiner">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="categroy-container">
+        {/* <img/> */}
+        <div className="category-body-conatiner">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
