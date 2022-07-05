cb5b3996a0190d5c806f8073e9a3dc7da79c126e
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-07-05 15:06:57 +0530

    added cart context and shopping icon and cart dropdown

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,17 +2,18 @@ import { useContext } from "react";
 
 import { ProductsContext } from "../../contexts/products.context";
 
+import ProductCard from "../../components/product-card/product-card.component";
+
+import "./shop.styles.scss";
+
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {products && products.map(({ id, name }) => {
-        return (
-          <div key={id}>
-            <h1>{name}</h1>
-          </div>
-        );
-      })}
+    <div className="products-container">
+      {products &&
+        products.map((product) => {
+          return <ProductCard key={product.id} product={product} />;
+        })}
     </div>
   );
 };
