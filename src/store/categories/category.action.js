0504ccdd6505c0673428279465b8f579ab33c987
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-09-17 11:05:58 +0530

    add redux saga and migrate category thunk to saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,7 +1,6 @@
 import { CATEGORIS_ACTION_TYPES } from "./category.types";
 
 import { createAction } from "../../utils/reducer/reducer.utls";
-import { getCollectionAndDocuments } from "../../utils/firebase/firebase.utils";
 
 export const fetchCategoriesStart = () => {
   return createAction(CATEGORIS_ACTION_TYPES.FETCH_CATEGORIES_START);
@@ -16,15 +15,4 @@ export const fetchCategoriesSuccess = (categoriesArray) => {
 
 export const fetchCategoriesFailed = (error) => {
   return createAction(CATEGORIS_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-};
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-  
-  try {
-    const categoriesArray = await getCollectionAndDocuments();
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed());
-  }
 };
