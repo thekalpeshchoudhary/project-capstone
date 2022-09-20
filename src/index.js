e4b7f01fa04b3b9f46751b5853e7e177bc60d753
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2022-09-20 17:43:42 +0530

    Env file addition and stripe integration on checkout page

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 
 import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import "./index.scss";
 
@@ -17,7 +19,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
